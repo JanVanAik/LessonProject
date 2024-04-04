f651c825fe24b1cc01ab499706433b9d9237850e
JanVanAik <ar.orlovskiy@gmail.com>
2024-04-04 21:23:40 +0300

    some work with forms + added template

---
+++ mainapp/forms.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django import forms
+from mainapp.models import User, Product, Order
+
+
+class OrderForm(forms.ModelForm):
+    order_datetime = forms.DateTimeField(widget=forms.DateTimeInput)
+    class Meta:
+        model = Order
+        fields = ('order_datetime', 'user_id', 'product_id', 'total_price')
