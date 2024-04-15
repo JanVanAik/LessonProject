e5da4c2cf8c467c2588f8445387cc8e3cf988421
JanVanAik <ar.orlovskiy@gmail.com>
2024-04-15 19:12:57 +0300

    updated settings and urls

--- LessonProject/settings.py
+++ LessonProject/settings.py
@@ -20,12 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#&_^-8q@7lzetce63@f(45ave1y^ez!+8&ocvkwf@lo9j66o_@'
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = ['127.0.0.1', 'JanVanAik.pythonanywhere.com,']
 
 
 # Application definition
@@ -122,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,7 +5,6 @@ app_name = "mainapp"
 
 
 urlpatterns = [
-    path('', index, name='main'),
     path('about/', about, name='about'),
     path('orders/', orders, name='orders'),
     path('image/', image, name='image'),
